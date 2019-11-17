db1c303fa7953400280270bb3d8f638a74d014cd
Fernando Britto <me@fernandobritto.com.br>
2019-11-17 15:19:20 -0200

    Initial commit

---
+++ slim-server/lib/Models/Order.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * Order
+ */
+namespace \Models;
+
+/*
+ * Order
+ */
+class Order {
+    /* @var int $id  */
+    private $id;
+/* @var int $productId  */
+    private $productId;
+/* @var int $quantity  */
+    private $quantity;
+/* @var DateTime $shipDate  */
+    private $shipDate;
+/* @var string $status Order Status */
+    private $status;
+/* @var Bool $complete  */
+    private $complete;
+}
